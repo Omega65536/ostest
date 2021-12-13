dbd368bdb1a92dc7e801a5ac55a142f6a798f1c0
Omega65536 <omega65536@protonmail.com6>
2021-12-13 18:47:53 +0100

    split into two asm files

--- boot.asm
+++ boot.asm
@@ -2,53 +2,37 @@
 
 jmp start
 
-; Prints a string and a newline
-; bx Address of the string
-println_string:
-	mov ah, 0xe
-	loop: ; Loop over the string and print the chars
-		mov al, [bx]
-		cmp al, 0
-		je return
-		int 0x10
-		inc bx
-		jmp loop
-
-	return: ; Newline 
-		mov al, 0xA
-		int 0x10
-		mov al, 0xD
-		int 0x10
-		ret
-
 start:
-	mov bx, _string_start
+	mov bx, string_start
 	call println_string
 
 main:
-	mov bx, _string_test
+	mov bx, string_test
 	call println_string
-	mov bx, _string_test2
+	mov bx, string_test2
 	call println_string
 
 exit:
-	mov bx, _string_exit
+	mov bx, string_exit
 	call println_string
-	ret
 	jmp $
 
-_string_test:
-	db "123 abc ABC", 0
 
-_string_test2:
+
+%include "io.asm"
+
+string_test:
+	db "This is a test string.", 0
+
+string_test2:
 	db "Hello, world!", 0
 
-_string_start:
+string_start:
 	db "[OS] Starting!", 0
 
-_string_exit:
+string_exit:
 	db "[OS] Exiting!", 0
 
 ; Padding
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xaa55
---
+++ io.asm
@@ -0,0 +1,24 @@
+; Prints a string
+; bx Address of the string
+print_string:
+	mov ah, 0xe
+	loop: ; Loop over the string and print the chars
+		mov al, [bx]
+		cmp al, 0
+		je return
+		int 0x10
+		inc bx
+		jmp loop
+	return: ; return
+		ret
+
+; Prints a string and a newline
+; bx Address of the string
+println_string:
+	mov ah, 0xe
+	call print_string
+	mov al, 0xA ; Print a carriage return
+	int 0x10
+	mov al, 0xD ; Print a newline
+	int 0x10
+	ret
